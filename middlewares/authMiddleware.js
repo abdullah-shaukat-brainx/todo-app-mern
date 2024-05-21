feaa60215f68d3a072c69d0686a29653a0ecf40e
abdullah-shaukat-brainx <166981476+abdullah-shaukat-brainx@users.noreply.github.com>
2024-05-21 13:16:33 +0500

    Used chain operator instead of simple dot at lines 9 and 10

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -6,8 +6,8 @@ const auth = (req, res, next) => {
     if (token) {
       token = token.split(" ")[1];
       let user = jwt.verify(token, SECRET_KEY);
-      req.userId = user.id;
-      req.userEmail = user.email;
+      req.userId = user?.id;
+      req.userEmail = user?.email;
     } else res.status(401).json({ error: "Unauthorized user" });
     next();
   } catch (e) {
