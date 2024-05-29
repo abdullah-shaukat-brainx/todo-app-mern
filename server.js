1ab356470b75abef31f2258328569d392c1b31ce
abdullah <abdullah.shaukat@brainxtech.com>
2024-05-29 14:25:44 +0500

    updated cors policy

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1", require("./routes"));
 mongoose
